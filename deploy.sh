15983db628b6f6277748f96cb9fa2b9d3933d5a1
Espen Thilesen <ethilesen@mac.com>
2018-12-21 10:30:33 +0100

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 curl -H "Content-Type: application/json" \
--X POST -u "apikey:6MiENwozVXYMxoZxTXBcndMKNehtAQAFbVUCdEG4bazL" \
+-X POST -u "apikey:" \
 -d @skill-Skatteetaten.json \
 "https://gateway.watsonplatform.net/assistant/api/v1/workspaces?version=2018-09-20"
